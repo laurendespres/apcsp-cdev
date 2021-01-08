beee895ecbd91c356684f3a978254adb57836b83
Lauren Despres <despresl3095@my.sduhsd.net>
2021-01-08 18:45:56 +0000

    sizeofvariables.c activity

--- sizeofvariables.c
+++ sizeofvariables.c
@@ -19,5 +19,13 @@ int main()
   double d = 123.456;
   //print value and size of double variable
   printf("float d value: %d and size: %lu bytes\n", d, sizeof(d));
+
+  float z = 13.789;
+  //print value and size of float variable
+  printf("float z value: %z and size: %lu bytes\n", z, sizeof(z));
+
+ int p = 13;
+  //print value and size of int variable
+  printf("int p value: %d and size: %lu bytes\n", p, sizeof(p));  
 }
 
